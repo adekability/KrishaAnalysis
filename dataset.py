5d2bf35995acc4feba974a7e9477624165cc64ef
adekability <adekability@mail.ru>
2023-04-20 17:14:39 +0600

    add: docstrings to dataset

--- dataset.py
+++ dataset.py
@@ -1,11 +1,15 @@
+""" dataset.py - module containing management with datasets"""
+
 import pandas
 from dataclasses import dataclass
 
 
 @dataclass
 class Dataset:
+    """ class Dataset - has filename field, and method to fetch data from excel, csv """
     filename: str
 
-    def get_data(self):
+    def fetch_dataframe(self):
+        """ fetch_dataframe - method fetching data from filename to pandas Dataframe"""
         dataframe = pandas.read_excel(self.filename)
         return dataframe
